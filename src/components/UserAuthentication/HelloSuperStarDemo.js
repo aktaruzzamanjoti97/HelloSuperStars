40cebe21ea47f2d0254b0d3129fdf14eaffb4c97
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-07-13 12:47:28 +0600

    Customized Login and Signup form

--- src/components/UserAuthentication/HelloSuperStarDemo.js
+++ src/components/UserAuthentication/HelloSuperStarDemo.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import HelloSuperStarLogo from '../../images/HelloSuperStarLogo.png'
-import {Button} from 'reactstrap';
-
+import { Button } from 'reactstrap';
+import '../../App.css';
 const HelloSuperStarDemo = () => {
           return (
-                    <>
-                    <div>
-                    <img src={HelloSuperStarLogo} alt="Hello Super Star Logo" />
+                    <div className="class-col col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
+                              <div>
+                                        <img src={HelloSuperStarLogo} alt="Hello Super Star Logo" />
+                              </div>
+                              <div>
+                                        <Button outline color="warning"> <i><ion-icon name="videocam-outline"></ion-icon></i> Virtual Tour</Button>
+                              </div>
+
                     </div>
-                    <div>
-                              <Button outline color="warning">warning</Button>
-                    </div>
-                              
-                    </>
           )
 }
 
