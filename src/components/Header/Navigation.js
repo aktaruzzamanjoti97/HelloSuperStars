c7d5ec73769ed066940301a1d30f9afb37e56b07
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-07-13 21:42:37 +0600

    SideNavbar CustomizedHomePage NavigationIconFixed

--- src/components/Header/Navigation.js
+++ src/components/Header/Navigation.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
 import { Navbar, NavbarBrand, Nav, NavItem, NavbarToggler, Collapse } from "reactstrap";
 import {Link} from "react-router-dom";
-
+import HomeIcon from '@material-ui/icons/Home';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 class Navigation extends Component{
     constructor(props){
         super(props);
@@ -24,7 +27,16 @@ class Navigation extends Component{
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav className="ml-auto" navbar>
                                 <NavItem>
-                                    <Link to="/" className="nav-link active">Home</Link>
+                                    <Link to="/" className="nav-link active"><HomeIcon/></Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link to="/chat" className="nav-link active"><ChatIcon/></Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link to="/" className="nav-link active"><NotificationsActiveIcon/></Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link to="/" className="nav-link active"><ShoppingCartIcon/></Link>
                                 </NavItem>
                                 <NavItem>
                                     <Link to="/signup" className="nav-link">Sign Up</Link>
