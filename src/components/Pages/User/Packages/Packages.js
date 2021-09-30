796debd1aff4545b383cd9b20d098124e1ffcbe5
Apurbo Kumar Anup <70365611+Apurbo-cse@users.noreply.github.com>
2021-09-30 15:15:47 +0600

    Package page 80%

--- src/components/Pages/User/Packages/Packages.js
+++ src/components/Pages/User/Packages/Packages.js
@@ -1,22 +1,24 @@
 import React from "react";
+import Star from '../../../../images/Normal User/star.png'
 import "../../../CSS/Packages/package.css";
 
 export const Packages = () => {
   return (
     <>
       <div className="Package-container ">
-        <div className="container ">
-          <div className="row ">
-            <div className="col-md-3 col-sm-12 package-bg my-5 ">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3 col-sm-6 package-bg my-5 ">
               <div className="pak-b">
                 <div className="pa-header">
                   <p>Free</p>
                 </div>
                 <div className="package">
-                  <p>One Free Voice Record</p>
-                  <p>One Free Voice Record</p>
-                  <p>One Free Voice Record</p>
-                  <p>One Free Voice Record</p>
+                  <p> <img src={Star} className="star" alt="star" /> One Free Voice Record</p>
+                  <p><img src={Star} className="star" alt="star" /> One Free Video Upload</p>
+                  <p><img src={Star} className="star" alt="star" /> One Entry in One Fan Group</p>
+                  <p><img src={Star} className="star" alt="star" /> Free E-commerce Surfing</p>
+                  <p><img src={Star} className="star" alt="star" /> </p>
                 </div>
                 <div className="but-c">
                   <button className="package-btn">Free User</button>
@@ -27,13 +29,14 @@ export const Packages = () => {
             <div className="col-md-3 col-sm-12 package-bg my-5 ">
               <div className="pak-b">
                 <div className="pa-header">
-                  <p>Free</p>
+                  <p>Silver</p>
                 </div>
                 <div className="package">
-                  <p>One Free Voice Record</p>
-                  <p>One Free Voice Record</p>
-                  <p>One Free Voice Record</p>
-                  <p>One Free Voice Record</p>
+                  <p><img src={Star} className="star" alt="star" /> 10% off.On Every Purchase.</p>
+                  <p><img src={Star} className="star" alt="star" /> One Free Chat With A Star</p>
+                  <p><img src={Star} className="star" alt="star" /> One Free Video Upload</p>
+                  <p><img src={Star} className="star" alt="star" /> One Entry in One Fan Group</p>
+                  <p><img src={Star} className="star" alt="star" /> Free E-commerce Surfing</p>
                 </div>
                 <div className="but-c">
                   <button className="package-btn">Buy Now</button>
@@ -44,13 +47,13 @@ export const Packages = () => {
             <div className="col-md-3 col-sm-12 package-bg my-5 ">
               <div className="pak-b">
                 <div className="pa-header">
-                  <p>Free</p>
+                  <p> Gold</p>
                 </div>
                 <div className="package">
-                  <p>One Free Voice Record</p>
-                  <p>One Free Voice Record</p>
-                  <p>One Free Voice Record</p>
-                  <p>One Free Voice Record</p>
+                  <p><img src={Star} className="star" alt="star" /> One Free Voice Record</p>
+                  <p><img src={Star} className="star" alt="star" /> One Free Voice Record</p>
+                  <p><img src={Star} className="star" alt="star" /> One Free Voice Record</p>
+                  <p><img src={Star} className="star" alt="star" /> One Free Voice Record</p>
                 </div>
                 <div className="but-c">
                   <button className="package-btn">Buy Now</button>
@@ -61,13 +64,13 @@ export const Packages = () => {
             <div className="col-md-3 col-sm-12 package-bg my-5 ">
               <div className="pak-b">
                 <div className="pa-header">
-                  <p>Free</p>
+                  <p>Platinum</p>
                 </div>
                 <div className="package">
-                  <p>One Free Voice Record</p>
-                  <p>One Free Voice Record</p>
-                  <p>One Free Voice Record</p>
-                  <p>One Free Voice Record</p>
+                  <p><img src={Star} className="star" alt="star" /> One Free Voice Record</p>
+                  <p><img src={Star} className="star" alt="star" /> One Free Voice Record</p>
+                  <p><img src={Star} className="star" alt="star" /> One Free Voice Record</p>
+                  <p><img src={Star} className="star" alt="star" /> One Free Voice Record</p>
                 </div>
                 <div className="but-c">
                   <button className="package-btn">Buy Now</button>
