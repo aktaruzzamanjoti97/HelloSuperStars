a6fa7938d1c288cf364139a46c0e2d7874aa0ac2
Raihan <mizanurcse1452@gmail.com>
2021-10-03 10:55:58 +0600

    Firebase add

--- src/components/Database/Firebase.js
+++ src/components/Database/Firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const app= firebase.initializeApp({
-          apiKey: "AIzaSyCB8JJqFUCVO8CQFUW2SGCGQGpPzRZ1q0I",
-          authDomain: "hellosuperstar-userauth.firebaseapp.com",
-          projectId: "hellosuperstar-userauth",
-          storageBucket: "hellosuperstar-userauth.appspot.com",
-          messagingSenderId: "969718069500",
-          appId: "1:969718069500:web:34c3d7f78cd3b40f8d7f79"
+    apiKey: "AIzaSyAu-QJ-mc7g4Usx-fEMNILJp0AkiYmchXo",
+    authDomain: "hellosuperstars-7b616.firebaseapp.com",
+    projectId: "hellosuperstars-7b616",
+    storageBucket: "hellosuperstars-7b616.appspot.com",
+    messagingSenderId: "954435451043",
+    appId: "1:954435451043:web:4bf6274906102437e2a716"
 });
 
 const auth= app.auth();
