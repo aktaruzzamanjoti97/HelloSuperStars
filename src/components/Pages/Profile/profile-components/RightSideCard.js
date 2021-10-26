ef5c8845c381adcaacfaa3ba9bf484ae167c93d3
Md. Al Amin <48113312+alaminhossain171@users.noreply.github.com>
2021-10-26 15:56:56 +0600

    profile responsive problem solved

--- src/components/Pages/Profile/profile-components/RightSideCard.js
+++ src/components/Pages/Profile/profile-components/RightSideCard.js
@@ -50,7 +50,7 @@ const RightSideCard = (props) => {
                 <div className="left-text">
                 {/* <i className="fas fa-share d-inline text-light mx-1"></i> */}
                 
-                <small className="d-inline profile-card-text">260 Share</small>
+                <small className="d-inline profile-card-text">250 Share</small>
                 </div>
               </div>
             </div>
