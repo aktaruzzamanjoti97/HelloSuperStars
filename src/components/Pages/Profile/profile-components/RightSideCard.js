db32fb2ad0622792eaa0655a3589a0371034d250
Md. Al Amin <48113312+alaminhossain171@users.noreply.github.com>
2021-10-26 15:51:17 +0600

    update rightside card

--- src/components/Pages/Profile/profile-components/RightSideCard.js
+++ src/components/Pages/Profile/profile-components/RightSideCard.js
@@ -13,7 +13,7 @@ const RightSideCard = (props) => {
                 src={props.profileLogo}
                 alt=""
                 className="img-fluid rounded-circle"
-                width="50px"
+                width="60px"
               />
             </div>
             <div className="col-10 mt-2">
