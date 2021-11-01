f61a62833b6e752ada022b2b26a30476e605a6b2
Apurbo Kumar Anup <apurboka@gmail.com>
2021-11-01 13:45:54 +0600

    MarketPlace Pro

--- src/components/Pages/Market/Content/Content.js
+++ src/components/Pages/Market/Content/Content.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Carousel from "../../Market/Content/Sub/Carosuel";
+import Profile from "../../Market/Content/Sub/Profile";
+
+
 export const Content = () => {
   return (
     <>
         <div className="MarketPlace">
+
             <div className="Carosuel-mp col-md-10">
               <Carousel/>
             </div>
+
+            <div className="Carosuel-mp col-md-10 Post-mp-bg d-flex">
+              <Profile/>
+            </div>
+
         
         </div>
     </>
