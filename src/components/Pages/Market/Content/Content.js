89e1028e066ee37e36747377c0d6d3a63b87e00a
Apurbo Kumar Anup <apurboka@gmail.com>
2021-11-01 12:06:01 +0600

    MarketPlace Carosuel Done

--- src/components/Pages/Market/Content/Content.js
+++ src/components/Pages/Market/Content/Content.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Carousel from "../../Market/Content/Sub/Carosuel";
 export const Content = () => {
   return (
     <>
-      fgdfgdf
+        <div className="MarketPlace">
+            <div className="Carosuel-mp col-md-10">
+              <Carousel/>
+            </div>
+        
+        </div>
     </>
   );
 };
