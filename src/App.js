90792d7a307f7b42c92a69ba7c743fd6e95cb7dd
Apurbo Kumar Anup <apurboka@gmail.com>
2021-11-02 17:19:03 +0600

    Learn Session

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import {Route, Switch, BrowserRouter} from "react-router-dom";
-import Home from "./components/Pages/Home/Body/HomePages";
+
+
 import Login from "./components/UserAuthentication/Login";
 import Signup from "./components/UserAuthentication/Signup";
 import Otp from "./components/UserAuthentication/Otp";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
-import Navigation from './components/Header/Navigation';
 import HelloSuperStarDemo from './components/UserAuthentication/HelloSuperStarDemo';
 import AccountCreate from "./components/UserAuthentication/AccountCreate";
 import SliderTutorial from "./components/Pages/User/TutorialSPage/SliderTutroial";
@@ -14,9 +14,18 @@ import Packages from "./components/Pages/User/Packages/Packages";
 import BuyPackages from "./components/Pages/User/Packages/BuyPackages";
 import Profile from './components/Pages/Profile/Profile';
 
+// Navigation
+import Navigation from './components/Header/Navigation';
+
+// Home Page
+import Home from "./components/Pages/Home/Body/HomePages";
+import Live from "./components/Pages/Home/Body/LiveNow/LiveBody";
+import Learn from "./components/Pages/Home/Body/LeraningSessions/LearnBody";
+import UpLive from "./components/Pages/Home/Body/UpcomingLive/UpliveBody";
+import Auditions from "./components/Pages/Home/Body/UpcomingAuditions/AuditionsBody";
 
 // Market Place Page
-import MarketPlace from "./components/Pages/Market/MarketPlace";
+import Market from "./components/Pages/Market/MarketPlace";
 
 
 import './components/CSS/Navbar/LeftSideNavbar.css';
@@ -25,7 +34,6 @@ import './App.css';
 import GuestUserPage from "./components/Pages/User/GuestUser/GuestUserPage";
 import CoreCategory from "./components/Pages/Category/CoreCategory";
 import SubCategory from "./components/Pages/Category/SubCategory";
-import StarSelection from "./components/Pages/Category/StarSelection";
 
 
 function App() {
@@ -34,12 +42,20 @@ function App() {
     <BrowserRouter>
     <PrivateRoute component={Navigation} />
       <Switch>
+
         <PrivateRoute exact path='/' component={Home} />
-        <PrivateRoute exact path='/marketplace' component={MarketPlace} />
+        <PrivateRoute exact path='/live-now' component={Live} />
+        <PrivateRoute exact path='/learn-sessions' component={Learn} />
+        <PrivateRoute exact path='/upcoming-live' component={UpLive} />
+        <PrivateRoute exact path='/upcoming-auditions' component={Auditions} />
+
+        <PrivateRoute exact path='/marketplace' component={Market} />
+
         <Route exact path='/login' component={Login} />
         <Route exact path='/signup' component={Signup} />
         <Route exact path='/otp' component={Otp} />
         <Route path='/accountCreate' exact component={AccountCreate} />
+
         <Route exact path='/tutorial' component={SliderTutorial} />
         <Route exact path='/packages' component={Packages} />
         <Route exact path='/buy-packages' component={BuyPackages} />
@@ -47,7 +63,6 @@ function App() {
         <Route path='/profile' exact component={Profile} />
         <Route path='/coreCategory' exact component={CoreCategory} />
         <Route path='/subCategory' exact component={SubCategory} />
-        <Route path='/starSelection' exact component={StarSelection} />
         <PrivateRoute exact path='/logo' component={HelloSuperStarDemo} />
       </Switch>
     </BrowserRouter>
