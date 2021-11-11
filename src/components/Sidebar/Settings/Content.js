938486cd14b65070e0723392c6a66aba881be669
Apurbo Kumar Anup <apurboka@gmail.com>
2021-11-11 17:41:18 +0600

    Following 80%

---
+++ src/components/Sidebar/Settings/Content.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Content = () => {
+    return (
+        <>
+            <div className=" Follow-Category">
+                <h1>Settings</h1>
+            </div>
+           
+        </>
+    )
+}
+
+export default Content
