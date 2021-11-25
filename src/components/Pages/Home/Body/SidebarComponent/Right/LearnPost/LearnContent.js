6b4cabab1fc29aef55e7914bce691b416ede0c41
Apurbo Kumar Anup <apurboka@gmail.com>
2021-11-25 13:13:58 +0600

    Post CSS Update

--- src/components/Pages/Home/Body/SidebarComponent/Right/LearnPost/LearnContent.js
+++ src/components/Pages/Home/Body/SidebarComponent/Right/LearnPost/LearnContent.js
@@ -1,64 +1,73 @@
-import React from 'react'
+import React from "react";
 
-const LearnContent = (props) => {
-    return (
-        <>
-        <div className="Post-Scroll-S col-md-10 align-items-center ml-2">
-              <div className="card left-col-box PostCard mb-3 mt-3">
-                  <div className="accordion-item PostBack ">
-                      <h2 className="accordion-header PostBack" >
-                          <div className="accordion-button-fx profile1-accordion-button PostBack  collapsed">
-                              <img src={props.ProfileImg} className="PostImgHome" alt="..." />
-                              <span className="mx-2 text-warning text-light">{props.Name}<br></br>
-                                  <small className="category-size-chat "> <span className="Post-small-text">{props.Time}</span></small>
-                                  <small className="category-size-chat "> <span className="Post-small-text-r">{props.Date}</span></small>
-                              </span>
-                          </div>
-                      </h2>
-                  </div>
-                  <div className="container my-2 Post-Title-home">
-                      {props.Msg}
-                  </div>
+    export default function LearnContent({user}){
+        return (
+          <>
+               <div className="container align-items-center justify-content-center col-11 Enroll-Auditions">
+                   
+                  <div className="card Enroll-AuditionsB">
 
-                  <img src={props.PostImg} className="card-img-top img-fluid mx-auto PostImgHe" alt="..." />
+                        <h2 className="accordion-header PostBack" >
+                            <div className="accordion-button-fx profile1-accordion-button PostBack  collapsed">
 
-                  <div className="card-body">
-                      <div className="container">
-                          <ul className="PostHoUl">
-                              <li className="like-post"> 
-                                  <i className="fas fa-heart heart mx-1 "></i>
-                                  <small className="profile-card-text">{props.LikeCount}</small>
-                              </li>
-                              <li className="share-post">
-                                  <small className="profile-card-text">{props.ShareCount} Share</small>
-                              </li>
-                              <li className="comment-post">
-                                  <small className=" profile-card-text">{props.CommentCount} Comment</small>
-                              </li>
-                            
-                          </ul>
-                      </div>
-                      <hr class="new5" />
+                                  <img src={user.profilePicture} className="PostImgHome" alt={user.profilePicture} />
+                                  <span className="mx-2 text-warning text-light">{user.username}<br></br>
+                                      <small className="category-size-chat"> <span className="Post-small-text">{user.time}</span></small>
+                                      <small className="category-size-chat"> <span className="Post-small-text-r">{user.date}</span></small>
+                                  </span>
+
+                             </div>
+                        </h2>
+
+                        <div className="my-2 Enroll-a Enroll-text">
+                            {user.postTitel}
+                        </div>
   
-                      <div className="text-center">
-                          <button className="btn-warning-post ">
-                              <i className="fas fa-heart text-danger  mx-1"></i>
-                              <small className="Post-Title-home"> Like</small>
-                          </button>
-                          <button className="btn-warning-post mx-2 ">
-                              <i className="fas fa-comment  mx-1 "></i>
-                              <small> Comment</small>
-                          </button>
-                          <button className="btn-warning-post">
-                              <i className="fas fa-share  mx-1"></i>
-                              <small> Share</small>
-                          </button>
-                      </div>
+                        <div className="card PostCard">
+                            <img src={user.postPic} alt={user.postPic} />
+                        </div>
+  
+                        <div className="row align-items-center justify-content-center ">
+                              <div className="container ">
+                                  <ul className="PostHoUl Co-Auditions">
+                                      <li className="like-post"> 
+                                          <i className="fas fa-heart heart mx-1 "></i>
+                                          <small className="profile-card-text">{user.LikeCount}</small>
+                                      </li>
+                                      <li className="share-post">
+                                          <small className="profile-card-text">{user.ShareCount} Share</small>
+                                      </li>
+                                      <li className="comment-post">
+                                          <small className=" profile-card-text">{user.CommentCount} Comment</small>
+                                      </li>
+  
+                                  </ul>
+                              </div>
+                  
+    
+                              <div className="text-center hr-Auditions">
+                                  <button className="btn-warning-post ">
+                                      <i className="fas fa-heart text-danger  mx-1"></i>
+                                      <small className="Post-Title-home"> Like</small>
+                                  </button>
+                                  <button className="btn-warning-post mx-2 ">
+                                      <i className="fas fa-comment  mx-1 "></i>
+                                      <small> Comment</small>
+                                  </button>
+                                  <button className="btn-warning-post">
+                                      <i className="fas fa-share  mx-1"></i>
+                                      <small> Share</small>
+                                  </button>
+                              </div>
+  
+  
+                          </div>
+
+                      
                   </div>
               </div>
-          </div>
-        </>
-    )
-}
-
-export default LearnContent
+          </>
+        );
+      };
+      
+     
--- src/components/Pages/Home/Body/SidebarComponent/Right/LearnPost/LearnPost.js
+++ src/components/Pages/Home/Body/SidebarComponent/Right/LearnPost/LearnPost.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import Navigation from "../../../../../../Header/Navigation";
 import LeftSidebar from '../../../../../../Sidebar/LeftSidebar'
 import RightSidebar from '../../../../../../Sidebar/RightSidebar'
-import Learn from "./LearnData"
+import Learn from "./Learn";
+
+
 export default class LearnPost extends Component {
   render() {
     return (
