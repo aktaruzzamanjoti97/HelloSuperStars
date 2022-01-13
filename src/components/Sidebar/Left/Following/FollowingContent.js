8c5b20e09f4f6415a3d357854c6ff2b8a41abb89
Apurbo Kumar Anup <apurboka@gmail.com>
2022-01-13 16:24:22 +0600

    Update FollowingContent.js

--- src/components/Sidebar/Left/Following/FollowingContent.js
+++ src/components/Sidebar/Left/Following/FollowingContent.js
@@ -1,28 +1,38 @@
-import  { React, useState}  from "react";
+import { React, useState} from "react";
 import Football from "../../../../images/Market/Football.jpg";
 import Plus from '../../../../images/Normal-User/add.png'
 const FollowingContent = () => {
-  
+const [value, setValue] = useState(true);
+const [btn, setBtn] = useState("Follow");
 
-  const [buttonText, setButtonText] = useState("Follow"); 
-  const changeText = (text) => setButtonText(text);
+return (
+<>
+  <div className="ProMain row col-md-4">
+    <div className="Follow-Category-div">
+      <img src={Football} className="Follow-Category-Img" alt="" />
+      <span className="Follow-Category-text">Dwane Johnson</span>
+    </div>
 
+    <button className="Follow-Category-text bt-cat-fol" type="button" onClick={()=> {
+      setValue(!value);
+      if (value === true) {
+      setBtn("Unfollow");
+      }
+      else {
+      setBtn("Follow");
+      }
+      }}
+      style={
+      value ? { backgroundColor: "gray" } : { backgroundColor: "#FFAD00" }
+      }
+      >
+      <img src={Plus} className="plus-cat" alt="" /> {btn}
 
-  return (
-    <>
-          <div className="ProMain row col-md-4">
-            <div className="Follow-Category-div">
-              <img src={Football} className="Follow-Category-Img" alt="" />
-              <span className="Follow-Category-text">Dwane Johnson</span>
-            </div>
-            <button className="Follow-Category-text bt-cat-fol" type="button"
-             onClick={() => changeText("Unfollow")}><img src={Plus} className="plus-cat" alt="" />{buttonText}</button>
-          </div>
+    </button>
+  </div>
 
-    </>
-  );
+</>
+);
 };
 
 export default FollowingContent;
-
-
