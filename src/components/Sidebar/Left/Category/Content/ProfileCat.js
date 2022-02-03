26936cca91f9c2d0e630ae01759ba03676b63aa9
M. Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2022-02-03 15:19:57 +0600

    Dynamic follow block issues solved

--- src/components/Sidebar/Left/Category/Content/ProfileCat.js
+++ src/components/Sidebar/Left/Category/Content/ProfileCat.js
@@ -8,6 +8,7 @@ import haaland from '../../../../../images/haaland.jpg';
 import './ProfileCat.css';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import { Dropdown } from 'react-bootstrap';
+import ProfileCatMap from './ProfileCatMap';
 
 const ProfileCat = () => {
 
@@ -163,52 +164,8 @@ const ProfileCat = () => {
                 {
                     data.map((singleData) => {
                         return (
-                            <div className="col-md-6">
-                                <div className="star-Card starCardBorder my-3">
-                                    <div className="row">
-                                        <div className="col-md-6 col-sm-6 cardFirstSection">
-                                            <div className="mt-5 pt-4">
-                                                <div className="d-flex flex-column justify-content-center align-items-center">
-                                                    <div className="followButton mt-5">
-                                                        <button onClick={handleClick} type="button" className="btn btn-success text-center"><i className="fas fa-user-plus "></i> {toggleButton.text}</button>
-                                                    </div>
 
-                                                    <div className=" blockButton mt-5">
-                                                        <button onClick={handleBlock} type="button" className="btn btn-danger text-center"><i className="fas fa-user-minus "></i> {blockButton.text}</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-md-6 col-sm-6">
-                                            <div className="d-flex">
-                                                <div className="d-flex justify-content-center align-items-center">
-                                                    <div className="hexagonBox">
-                                                        <div class="hexagon"></div>
-
-                                                        <div className="starProfileImage">
-                                                            <div className="d-flex justify-content-center">
-                                                                <div>
-                                                                    <img className="img-fluid starCardPro text-center" src={singleData.bgImg} alt="" />
-                                                                    <h5 className="nameStar text-white text-center text-bold text-bold mt-1"><small>{singleData.playerName}</small></h5>
-                                                                </div>
-                                                            </div>
-
-                                                        </div>
-                                                    </div>
-
-                                                </div>
-
-                                                <div className="jotiDiv">
-
-                                                </div>
-
-                                            </div>
-
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            <ProfileCatMap singleData={singleData} key={singleData.id} />
                         )
                     })
                 }
