f93343ba3ae4d47f56d5163b5b6a40caf68c3c35
Sarwar Jahan Shohan <sarwar.shohan24@gmail.com>
2022-02-07 18:59:52 +0600

    greetings register  delete retry

--- src/components/Pages/Profile/profile-components/starProfile/StarChat/starGreeeting/Greeting.js
+++ src/components/Pages/Profile/profile-components/starProfile/StarChat/starGreeeting/Greeting.js
@@ -1,3 +1,4 @@
+
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import DateTimePicker from "@mui/lab/DateTimePicker";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
@@ -10,11 +11,14 @@ import ReactPlayer from 'react-player';
 import swal from "sweetalert";
 import StarProfileRightContent from "../../StarCardComponent/StarProfileRightContent/StarProfileRightContent";
 import "./Greeting.css";
+import { Markup } from 'interweave';
+
 
 const Greeting = ({ star_id }) => {
-  const [check, setcheck] = useState(true);
+  // const [check, setcheck] = useState(false);
   const [startTime, setStartTime] = useState(new Date());
   const [greeting, setGreeting] = useState(null);
+  const [GreetingInfo, setGreetingInfo] = useState({});
   const [status, setStatus] = useState({
     action: true,
     msg : ''
@@ -22,23 +26,44 @@ const Greeting = ({ star_id }) => {
 
 
   useEffect(() => {
-
+  
+    // setcheck(true)
+    GreetingsRegStatus()
+    
       axios.get("/sanctum/csrf-cookie").then((response) => {
-      axios.get(`/api/user/greetings_registaion_status/${star_id}`).then((res) => {
-        if (res.data.status === 200) {
-          console.log(res.data.greeting);
-          setGreeting(res.data.greeting);
-          if (res.data.greeting) {
-            setcheck(false)
+        axios.get('/api/user/greetings_star_status/'+star_id).then((res) => {
+          if (res.data.status === 200) {
+        
+              setGreetingInfo(res.data.greeting);
+          } else {
+            swal("error", "Data base Error", "error");
           }
+        });
+      });
+      
+
+  }, []);
+  
+  let GreetingsRegStatus = () => {
+    axios.get("/sanctum/csrf-cookie").then((response) => {
+      axios.get("/api/user/greetings_registaion_status").then((res) => {
+        if (res.data.status === 200) {
+     
+          setGreeting(res.data.greeting);
+          // setcheck(res.data.action)
+
+        
           if (res.data.greeting.status == 0) {
             setStatus({
              ...status,
               msg: 'Pending'
             })
-         
+            // setcheck(true)
           } else {
-            setStatus({msg:'Appreoved'})
+            setStatus({
+              ...status,
+              msg: 'Appreoved'
+            })
        
           }
     
@@ -47,24 +72,28 @@ const Greeting = ({ star_id }) => {
 
         }
       });
-    });
-  },[]);
+      });
+  }
+
 
   function handelTimeSubmit(e) {
-    setcheck(false)
+    // setcheck(true)
     e.preventDefault();
 
     let Form_data = new FormData(e.target)
     Form_data.append('time', moment(startTime));
-    Form_data.append('greetings_id', star_id);
+    Form_data.append('greetings_id', GreetingInfo.id);
 
     axios.get("/sanctum/csrf-cookie").then((response) => {
       axios.post(`api/user/greetings_registaion`, Form_data).then((res) => {
         if (res.data.status === 200) {
+        
+          setGreeting(res.data.greeting);
+          // setcheck(true)
 
           //document.getElementById('input_form').reset();
           swal("Success", res.data.message, "success");
-          setGreeting(res.data.greeting);
+
           if (res.data.greeting.status == 0) {
             setStatus({
               ...status,
@@ -82,6 +111,40 @@ const Greeting = ({ star_id }) => {
     });
 
 
+  }
+
+  /**
+   * delete register grettings 
+   */
+  let handelGreetingsRegDelete = () => {
+    swal({
+      title: "Are you sure?",
+      text: "You Want to Delete your Greetings",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    })
+    .then((willDelete) => {
+      if (willDelete) {
+          axios.get("/sanctum/csrf-cookie").then((response) => {
+          axios.get("api/user/greetings_reg_delete/"+greeting.id).then((res) => {
+            if (res.data.status === 200) {
+            GreetingsRegStatus()
+            swal("Your Greetings is Delete", {
+              icon: "success",
+            });
+
+            } else {
+              swal("error", "hello", "error");
+
+            }
+          });
+        });
+      
+      } else {
+        // swal("Your Greetings is safe!");
+      }
+    });
   }
   return (
     <>
@@ -112,43 +175,34 @@ const Greeting = ({ star_id }) => {
 
               </div>
             </div>
-
+            {GreetingInfo ?
+       
             <div className="text-warning">
               <div class="card live-card-bg my-4 mx-auto">
                 <div class="card-body">
                   <h5 class="card-title text-warning">Instructions</h5>
                   <div className="star-line"></div>
                   <div className="instruct">
-                    <p class=" my-4 text-light">
-                      1. User can only chat with the superstar for minimum 1
-                      minute to maximum 5 minutes.
-                    </p>
-                    <p class=" my-4 text-light">
-                      2. User should not insult superstar or speak about their
-                      personal topics. There should be no insults or blasphemy
-                      with a superstar.
-                    </p>
-                    <p class=" my-4 text-light">
-                      3. User should proofread the chat to superstar before
-                      sending it to superstar{" "}
-                    </p>
+                      <Markup content={GreetingInfo.description}/>
                   </div>
                 </div>
               </div>
             </div>
+          :""}
 
-
+          {GreetingInfo ?
+       
             <div class="card live-card-bg my-4 mx-auto">
               <div class="card-body">
                 <h5 class="card-title text-warning">Cost</h5>
                 <div className="star-line"></div>
                 <div className="instruct">
-                  <h5 className='my-2 text-light'>200 BDT</h5>
+                    <h5 className='my-2 text-light'>{GreetingInfo.cost} BDT</h5>
                 </div>
               </div>
             </div>
 
-
+          :""}
 
             <div class="card live-card-bg my-4 mx-auto">
               <div class="card-body">
@@ -183,10 +237,10 @@ const Greeting = ({ star_id }) => {
                           {status.action ?
                             
                           <div className="d-flex justify-content-between mt-4 mb-3">
-                            <button className="btn btn-warning ms-4">
+                            <button className="btn btn-warning ms-4" onClick={handelGreetingsRegDelete}>
                               <i className="fas fa-redo-alt"></i> Retry
                             </button>
-                            <button className="btn btn-warning me-4">
+                            <button className="btn btn-warning me-4" onClick={handelGreetingsRegDelete}>
                               <i class="fas fa-trash-alt"></i> Delete
                             </button>
                           </div>
@@ -223,12 +277,11 @@ const Greeting = ({ star_id }) => {
                       </LocalizationProvider>
                     </div>
                     <div className="col-6 px-3">
-                      {check ?
-
-                        <button className='my-3 btn btn-warning px-4 py-2' type="submit" >Apply Now!</button>
-                        :
+                      {greeting ?
 
                         <button className='my-3 btn btn-success px-4 py-2' disabled>Applied <i class="fas fa-check-circle mx-1"></i></button>
+                        :
+                        <button className='my-3 btn btn-warning px-4 py-2' type="submit" >Apply Now!</button>
                       }
 
                     </div>
