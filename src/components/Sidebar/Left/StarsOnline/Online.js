21bd03e077e02970903358d1212e8c2649819eb6
Apurbo Kumar Anup <apurboka@gmail.com>
2022-02-22 15:13:00 +0600

    Star Online

--- src/components/Sidebar/Left/StarsOnline/Online.js
+++ src/components/Sidebar/Left/StarsOnline/Online.js
@@ -1,19 +1,30 @@
 import React from 'react'
 
 export default function Online({user}) {
-    return (
-        <>
-            <div className="accordion-header chatListname" id="headingTwo">
-            
-                <span> 
-                    <img src={user.profilePicture} alt="" className="SidebarStarListImage"/>
-                </span>
-                
-                <span className="OnlineGreen"><i class="fas fa-circle"></i></span>
-                <span className='chatName'>{user.username}</span>
-               
-            </div>  
-        </>
-    )
-}
+return (
+<>
 
+    <div className="accordion-header chatListname" id="headingTwo">
+
+        <div className="d-flex">
+            <div className="containerOnline">
+
+                <img src={user.profilePicture} className=" SidebarStarListImage" alt="profilePicture" />
+
+                <div class="onLineMo">🟢</div>
+
+            </div>
+            <div className="col-md-9  mt-2">
+                <span className='chatName'>{user.username}</span>
+
+            </div>
+        </div>
+
+        <span>
+
+        </span>
+
+    </div>
+</>
+)
+}
