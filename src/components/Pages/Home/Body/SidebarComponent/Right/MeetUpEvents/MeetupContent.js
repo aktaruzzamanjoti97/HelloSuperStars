cd0cbc0d1515ccb01f3fadd84ec71438d9068cfe
Apurbo Kumar Anup <apurboka@gmail.com>
2022-02-22 18:18:29 +0600

    Update MeetupContent.js

--- src/components/Pages/Home/Body/SidebarComponent/Right/MeetUpEvents/MeetupContent.js
+++ src/components/Pages/Home/Body/SidebarComponent/Right/MeetUpEvents/MeetupContent.js
@@ -66,22 +66,7 @@ return (
                         </Link>
                     </div>
                 </div>
-
-                {/* <div className="centered centered-meet">
-                    <div className="meetP  d-flex">
-                        <div className="MeetupText col-md-8 fw-bold">
-                            {event.title}
-                        </div>
-                        <div className="Meetupbtn col-md-3 align-items-center justify-content-center">
-
-                            <Link to={`starprofile/${event.star_id}/meetup-events/book_now/${event.id}`}
-                                className="link-starPorfile">
-                            <button className="btn btn-warning text-light fw-bold bmt">Book Now</button>
-                            </Link>
-
-                        </div>
-                    </div>
-                </div> */}
+                
                 
             </div>
 
@@ -127,99 +112,7 @@ return (
             </div>
         </div>
     </div>
-
-    {/* <div className="container align-items-center justify-content-center col-11 Enroll-Auditions">
-
-        <div className="card Enroll-AuditionsB">
-
-            <h2 className="accordion-header PostBack">
-                <div className="d-flex justify-content-between">
-                    <div className="accordion-button-fx profile1-accordion-button PostBack  collapsed">
-                        <img src={event.profilePicture} className="PostImgHome" alt={event.profilePicture} />
-                        <span className="mx-2 text-warning text-light">
-                            <Link to="/starprofile" className="link-starPorfile">
-                            {event.username}
-                            </Link>
-                            <br></br>
-                            <small className="category-size-chat">
-                                {" "}
-                                <span className="Post-small-text">{event.time}</span>
-                            </small>
-                            <small className="category-size-chat">
-                                <span className="Post-small-text-r">{event.date}</span>
-                            </small>
-                        </span>
-                    </div>
-
-                    <div className="dropdown me-2 buttonBorderNone">
-                        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            <DotsHorizontalIcon className="h-5 w-5 text-blue-500" />
-                        </button>
-                        <ul className="dropdown-menu leftSideNotification" aria-labelledby="dropdownMenuButton1">
-                            <li className="dropdown-item turnNotification">
-                                Turn on notification
-                            </li>
-                            <li className="dropdown-item">
-                                Unfollow Shakib
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </h2>
-            <div className="my-2 Enroll-a Enroll-text">
-                {event.postTitel}
-            </div>
-            <div className="card PostCard">
-                <img className="learningImg" src={event.postPic} alt={event.postPic} />
-                <div className="learningLockPremiumImg d-flex align-items-center justify-content-center">
-                    <img src={learningLockPremium} className="img-fluid img-resize-LearnContent" alt="" />
-                </div>
-            </div>
-            <div className="row align-items-center justify-content-center ">
-                <div className="container ">
-                    <ul className="PostHoUl Co-Auditions">
-                        <li className="like-post">
-                            <i className="fas fa-heart heart mx-1 "></i>
-                            <small className="profile-card-text">{event.LikeCount}</small>
-                        </li>
-                        <li className="share-post">
-                            <small className="profile-card-text">{event.ShareCount} Share</small>
-                        </li>
-                        <li className="comment-post">
-                            <small className=" profile-card-text">{event.CommentCount} Comment</small>
-                        </li>
-
-                    </ul>
-                </div>
-
-                <div className="text-center hr-Auditions">
-                    <button className="btn-warning-post ">
-                        <i className="fas fa-heart text-danger  mx-1"></i>
-                        <small className="Post-Title-home"> Like</small>
-                    </button>
-                    <button className="btn-warning-post mx-2 " variant="link" onClick={()=> setOpen(!open)}
-                        aria-expanded={open} aria-controls="collapseID" >
-                        <i className="fas fa-comment  mx-1 "></i>
-                        <small> Comment</small>
-                    </button>
-                    <button className="btn-warning-post">
-                        <i className="fas fa-share  mx-1"></i>
-                        <small> Share</small>
-                    </button>
-                </div> */}
-
-                {/* Comment Section */}
-                {/* <Collapse in={open}>
-                    <div id="collapseID">
-                        <LearnComment />
-                    </div>
-                </Collapse>
-
-            </div>
-        </div> */}
-        {/*
-    </div> */}
+    
 
 </>
 );
