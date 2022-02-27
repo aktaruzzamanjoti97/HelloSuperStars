8c68081ce2cd5e674384734ac5885beeef37e440
Apurbo Kumar Anup <apurboka@gmail.com>
2022-02-27 10:23:10 +0600

    u

--- src/components/Header/Right/LiveNowMenu.js
+++ src/components/Header/Right/LiveNowMenu.js
@@ -14,7 +14,11 @@ console.log(Lives);
 return (
 <>
     <div className="mt-3 LiveMoBG">
-        <div className="text-light justify-content-between"> .</div> 
+        <div className="d-flex MobileMain justify-content-between mx-2">
+            <div className="text-light LeftTeI">Live Now</div>
+            <div className="text-light LeftTeP btn">Check More</div>
+        </div>
+
         <div className="">
             <OwlCarousel items={4} loop autoplay autoplayTimeout={3000} margin={0} dots={false}>
                 {Lives.map((l) => (
@@ -22,8 +26,6 @@ return (
                 ))}
 
             </OwlCarousel>
-
-
 
         </div>
     </div>
