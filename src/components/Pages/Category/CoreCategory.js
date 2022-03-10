ff3892e0b9f332afb02398068f3d43d086b946f1
M. Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2022-03-10 12:56:38 +0600

    Core Category Section Done

--- src/components/Pages/Category/CoreCategory.js
+++ src/components/Pages/Category/CoreCategory.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useHistory } from "react-router-dom";
 import swal from 'sweetalert';
-import frame from "../../../images/Normal-User/Single-frame.png";
+import frame from "../../../images/CHOOSE-CATEGORY-Box.png";
 import "../../CSS/Category/Category.css";
 import Navigation from "../../Header/Navigation";
 import "./components/CategoryBtn";
@@ -11,93 +11,129 @@ import './CoreCategory.css';
 const CoreCategory = () => {
 
   const [loading, setLoading] = useState(true);
-  const [categorylist, setCategorylist] = useState([]);
-  // console.log(categorylist);
+  const [categoryList, setCategoryList] = useState([]);
   const [selectCategory, setCategory] = useState([]);
-  console.log( 'selectcategory',selectCategory);
 
-  const [checkAll, setCheckAll]=useState(false);
-  const [checked, setChecked] = useState([]);
+  // const [checked, setChecked] = useState([]);
 
   const [isCheckAll, setIsCheckAll] = useState(false);
   const [isCheck, setIsCheck] = useState([]);
+  console.log('IsCheck State', isCheck);
+  console.log('SelectAll', isCheckAll);
 
-  console.log(checked)
+
 
   const history = useHistory();
+
+
+  // @TODO: Loading Category Data
+  useEffect(() => {
+    axios.get(`/api/view-category`).then(res => {
+      // console.log(res.data.category);
+      if (res.status === 200) {
+        setCategoryList(res.data.category)
+      }
+      setLoading(false);
+    });
+  }, []);
+
+
+  // @TODO: All Select Functionality
+  const handleSelectAll = () => {
+    setIsCheckAll(!isCheckAll);
+    setIsCheck(categoryList.map((li) => li.id));
+    if (isCheckAll) {
+      setIsCheck([])
+    }
+  }
+
+
+
+  // @TODO: Select Unselect Functionality
+  const handleClick = (e) => {
+    const { id, checked } = e.target;
+    const parseId = parseInt(id);
+    setIsCheck([...isCheck, parseId]);
+    if (!checked) {
+      setIsCheck(isCheck.filter((item) => item !== parseId));
+    }
+  };
+
+
+
+
+
 
   const handleInput = (e) => {
     e.persist();
     setCategory({ ...selectCategory, [e.target.name]: e.target.value });
   }
 
-  const handleToggle = (value) => {
+  // const handleToggle = (value) => {
 
-    const currentIndex = checked.indexOf(value);
+  //   const currentIndex = checked.indexOf(value);
 
-    const newChecked = [...checked];
+  //   const newChecked = [...checked];
 
-    if (currentIndex === -1) {
-      newChecked.push(value)
-    } else {
-      newChecked.splice(currentIndex, 1)
-    }
+  //   if (currentIndex === -1) {
+  //     newChecked.push(value)
+  //   } else {
+  //     newChecked.splice(currentIndex, 1)
+  //   }
 
-    setChecked(newChecked);
+  //   setChecked(newChecked);
 
-  }
+  // }
 
 
   const loginSubmit = (e) => {
-    e.preventDefault();
 
-    //console.log(Checked);
+    if (isCheck.length > 0) {
+      e.preventDefault();
 
-    const data = {
-      cat: checked
+      //console.log(Checked);
+
+      const data = {
+        cat: isCheck
+      }
+
+
+      axios.post(`/api/select_category`, data).then(res => {
+        if (res.data.status === 200) {
+          //swal("Success",res.data.message,"success");
+          //  console.log(res.data.length);
+          //localStorage.setItem('category', res.data.length);
+
+          //history.push('/subCategory');
+
+          history.push({
+            pathname: '/',
+            //search: '?query=abc',
+            //state: { detail: res.data.length }
+          });
+        }
+        else if (res.data.status === 401) {
+          swal("Warning", res.data.message, "warning");
+        }
+        else {
+          setCategory({ ...selectCategory, error_list: res.data.validation_errors });
+        }
+      });
     }
 
-
-    axios.post(`/api/select_category`, data).then(res => {
-      if (res.data.status === 200) {
-        //swal("Success",res.data.message,"success");
-        //  console.log(res.data.length);
-        //localStorage.setItem('category', res.data.length);
-
-        //history.push('/subCategory');
-
-        history.push({
-          pathname: '/',
-          //search: '?query=abc',
-          //state: { detail: res.data.length }
-        });
-      }
-      else if (res.data.status === 401) {
-        swal("Warning", res.data.message, "warning");
-      }
-      else {
-        setCategory({ ...selectCategory, error_list: res.data.validation_errors });
-      }
-    });
 
 
   }
 
 
 
+  const handleRoute = () => {
+    history.push('/')
+  }
 
 
-  useEffect(() => {
-    axios.get(`/api/view-category`).then(res => {
-      // console.log(res.data.category);
-      if (res.status === 200) {
-        setCategorylist(res.data.category)
-      }
-      setLoading(false);
-    });
-  }, []);
 
-  
+
 
   var viewCategory = "";
 
@@ -106,12 +142,12 @@ const CoreCategory = () => {
   }
   else {
     viewCategory =
-      categorylist.map((item, index) => {
+      categoryList.map((item, index) => {
         return (
           <div className="col-md-3  text-center" id="id_work_days">
 
             <label className=''>
-              <input name="checkbox" value={item.id} type="checkbox"  onChange={() => handleToggle(item.id)} />
+              <input key={item.id} id={item.id} name={item.name} type="checkbox" onChange={handleClick} checked={isCheck.includes(item.id)} />
               <span className='btn btn-warning core-category-btn my-3 category-core py-auto'>{item.name}</span>
             </label>
 
@@ -121,14 +157,6 @@ const CoreCategory = () => {
       });
   }
 
-  const handleClick = () => {
-    setCheckAll(!checkAll);
-    setChecked(categorylist.map((item) => item.id));
-
-    if(checkAll) {
-      setChecked([])
-    }
-  }
 
 
 
@@ -151,14 +179,14 @@ const CoreCategory = () => {
             {viewCategory}
           </div>
 
-          <div className="category-footer mt-4 ">
-            <h6 className=" text-center mt-2">
+          <div className="category-footer mt-5 ">
+            {/* <h6 className=" text-center mt-2">
               <i className="far fa-star text-warning mx-1"></i>{" "}
               <i className="far fa-star text-warning mx-1"></i>{" "}
               <span className="text-light">Choose at least 5 Categories</span>{" "}
               <i className="far fa-star text-warning mx-1"></i>{" "}
               <i className="far fa-star text-warning mx-1"></i>
-            </h6>
+            </h6> */}
 
 
 
@@ -167,8 +195,8 @@ const CoreCategory = () => {
               <div>
                 <div className='d-flex coreCategory'>
 
-                  <input  className='css-checkbox me-2' type="checkbox" id="weekday-2" name="weekday-2" onChange={handleClick} checked={checkAll} />
-                  <label className='selectAll' for="weekday-2">Select All</label>
+                  <input className='css-checkbox mx-2' type="checkbox" id="selectAll" name="selectAll" onChange={handleSelectAll} checked={isCheckAll} />
+                  <label className='selectAll' for="selectAll"><span className="ms-2">Select All</span></label>
 
                   {/* <p className="text-warning">Select All</p>
                   <input name="checkbox" className='checkboxInput' type="checkbox" id="" /> */}
@@ -177,13 +205,19 @@ const CoreCategory = () => {
               </div>
 
               <div>
+
                 <button onClick={loginSubmit} className="btn mt-3 mx-2 cg-done">Done</button>
-                {/* <Link to='/'> <button className="btn mt-3 mx-2 cg-skip">Skip</button> </Link> */}
               </div>
 
+
               <div>
-                <p className='text-warning'>Skip</p>
+                <div onClick={handleRoute} className='d-flex align-items-center coreCategorySkip px-4 py-1'>
+                  <div className='text-warning pt-1 skipFont'>Skip</div>
+                  <i className="fa-solid fa-arrow-right text-light pt-1 iconRight ms-2"></i>
+                </div>
               </div>
+
+
 
 
             </div>
