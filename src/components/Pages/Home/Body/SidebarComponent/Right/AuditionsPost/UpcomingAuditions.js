38445321c3ccbb1f3f250c2c0394d17ffa0eaceb
Abdul Kaiyum <52959794+A-Kaiyum@users.noreply.github.com>
2022-03-28 13:06:42 +0600

    audition published in the website

--- src/components/Pages/Home/Body/SidebarComponent/Right/AuditionsPost/UpcomingAuditions.js
+++ src/components/Pages/Home/Body/SidebarComponent/Right/AuditionsPost/UpcomingAuditions.js
@@ -1,10 +1,24 @@
-import React from 'react'
-import { Users } from '../../../../../../../DummyData';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+//import { Users } from '../../../../../../../DummyData';
 import AuditionsContent from './UpcomingAuditionsContent';
 
 
 const UpcomingAuditions = () => {
-    console.log(Users);
+
+    const[Users, setUpcomingAuditions] = useState([]);
+    useEffect(()=>{
+
+        axios.get("/api/user/getUpcomingAuditions").then((res) => {
+            if (res.data.status === 200) {
+                //console.log('upcomingAuditions',res.data.upcomingAuditions)
+                setUpcomingAuditions(res.data.upcomingAuditions);
+                
+            }
+  
+        });
+    },[])
+
     return (
         <>
             {Users.map((e) => (
