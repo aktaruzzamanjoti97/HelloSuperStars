9622d964b9d9a01cc5e37c66006d1ac1115fdc0b
Abdul Kaiyum <52959794+A-Kaiyum@users.noreply.github.com>
2022-04-04 15:50:51 +0600

    audition enrollment

--- src/components/Sidebar/Left/EnrolledAuditions/Enroll.js
+++ src/components/Sidebar/Left/EnrolledAuditions/Enroll.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import  {Users} from '../../../../DummyData'
 import EnrollContent from './EnrollContent';
 
 const Enroll = () => {
-    console.log(Users);
+
+
+    const [Users,setUsers] = useState([]);
+
+
+    useEffect(()=>{
+
+        axios.get('api/user/audition/enrolled').then(res =>{
+            if(res.data.status === 200)
+            {
+                console.log('Enroll Auditions ',res.data.enrolledAuditions);
+                setUsers(res.data.enrolledAuditions);
+
+            }          
+        });
+    },[])
+
+
     return (
         <>
             {Users.map((e) => (
