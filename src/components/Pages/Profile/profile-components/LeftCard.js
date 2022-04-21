53c989d14bf1229584c656fac2d8a7ae52d59c1a
Abdul Kaiyum <52959794+A-Kaiyum@users.noreply.github.com>
2022-04-21 16:02:32 +0600

    photos show in the user section

--- src/components/Pages/Profile/profile-components/LeftCard.js
+++ src/components/Pages/Profile/profile-components/LeftCard.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import blackBox from '../../../../images/Profile/blackbox.jpeg'
 const LeftCard = (props) => {
+
+    // console.log('props data',props.photos);
 return (
 <>
     <div className="container left-col-box py-4">
         <h6 className='profile-font-color'>{props.title}</h6>
         <div className="row">
+        {props?.photos?.map((data)=>(
+            <>
+           
             <div className="col-4">
-                <img src={blackBox} alt="" className='img-fluid profile-photos' />
+            {data.type == "learningSession" ?  <img src={`http://localhost:8000/${data?.learning_session?.banner}`} alt="" className='img-fluid profile-photos' /> : data.type == "meetup" ? <img src={`http://localhost:8000/${data?.meetup?.banner}`} alt="" className='img-fluid profile-photos' />  :null}
+               
             </div>
+            
+            </>
+        ))}
             <div className="col-4">
                 <img src={blackBox} alt="" className='img-fluid profile-photos' />
             </div>
